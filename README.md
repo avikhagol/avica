a056f7146ef166cc61025cb72e8208dd5b08ad80
Avinash Kumar <avialxee@gmail.com>
2023-07-04 18:55:02 +0300

    cli and readme

--- README.md
+++ README.md
@@ -3,3 +3,26 @@ VASCO: VLBI and SMILE source based CASA Optimizations
 
 This is a helper tool for CASA based optimizations to use rPICARD and CASA for VLBI in general.
 
+# USAGE
+```bash
+usage: vasco [-h] [-plist PARAMETER_LIST]
+
+VLBI and SMILE source based CASA Optimizations (VASCO).
+
+optional arguments:
+  -h, --help            show this help message and exit
+
+plotting:
+  use plotms based arguments to generate plots in the terminal.
+
+  -plist PARAMETER_LIST, --parameter-list PARAMETER_LIST
+                        list of parameters comma separated to fill in plotms
+```
+
+# Development
+After you clone the repo
+```bash
+
+$ cd vasco
+pip install -e .[dev]
+```
--- src/vasco/cli.py
+++ src/vasco/cli.py
@@ -4,13 +4,13 @@ from collections import defaultdict
 from pathlib import Path
 
 
-parser = argparse.ArgumentParser('scoobi',description="""
+parser = argparse.ArgumentParser('vasco',description="""
 VLBI and SMILE source based CASA Optimizations (VASCO).""", formatter_class=argparse.RawDescriptionHelpFormatter)
 
 
 plotting=parser.add_argument_group('plotting', """use plotms based arguments to generate plots in the terminal.""")
 
-plotting.add_argument('-plist', '--parameter-list', help="list of parameters in comma separated fashion")
+plotting.add_argument('-plist', '--parameter-list', help="list of parameters comma separated to fill in plotms")
 
 args=parser.parse_args()
 
