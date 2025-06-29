14f2f5138dc39c2f44153c13463c9d34677a3fb8
Kumar Avinash <akumar@ia.forth.gr>
2025-06-29 23:23:55 +0300

    0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as rdme:
 
 setup(
     name = 'vasco',
-    version = '0.1.5',
+    version = '0.2.0',
     url='https://gitlab.ia.forth.gr/smile/vasco/',
     author='Avinash Kumar',
     author_email='avialxee@gmail.com',
