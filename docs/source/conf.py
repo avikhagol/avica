5b8671cee4ddc7e042e9c76225da6a4bd94701c7
Avinash Kumar <avialxee@gmail.com>
2026-03-30 15:47:54 +0300

    docs under construction

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ release = '0.0.1'
 
 extensions = ['sphinx.ext.napoleon',
     'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
+    # 'sphinx.ext.viewcode',
     'sphinx.ext.coverage',
     'sphinx.ext.autosectionlabel',
     'jupyter_sphinx',
